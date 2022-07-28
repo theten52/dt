4a68e57a31b53cae26afb55c12db370272cfbe9e
wangj748 <jin.wang.-nd@disney.com>
2022-07-29 04:30:35 +0800

    init main method

--- go.mod
+++ go.mod
@@ -3,7 +3,16 @@ module dt
 go 1.18
 
 require (
-    github.com/cheggaaa/pb/v3 v3.0.5
-    github.com/fatih/color v1.10.0
-    github.com/urfave/cli/v2 v2.3.0
+	// github.com/cheggaaa/pb/v3 v3.0.5
+	github.com/fatih/color v1.10.0
+	github.com/urfave/cli/v2 v2.3.0
+)
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/russross/blackfriday/v2 v2.0.1 // indirect
+	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
 )
---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/fatih/color"
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	var model, token string
+
+	app := &cli.App{
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "model",
+				Usage:       "Operating mode; r:replace, b:backup",
+				DefaultText: "b",
+				Aliases:     []string{"m"},
+				Required:    true,
+				Destination: &model,
+			},
+			&cli.StringFlag{
+				Name:    "download-path",
+				Usage:   "The path where the image is stored",
+				Aliases: []string{"dp"},
+
+				Required: false,
+			},
+			&cli.StringFlag{
+				Name:    "markdown-path",
+				Usage:   "The path where the markdown file is stored",
+				Aliases: []string{"mp"},
+
+				Required: false,
+			},
+			&cli.StringFlag{
+				Name:        "token",
+				Usage:       "Upload token",
+				Aliases:     []string{"tk"},
+				Required:    false,
+				Destination: &token,
+			},
+		},
+		Action: func(c *cli.Context) error {
+
+			color.Yellow("Current Model is [%v]", "starting")
+
+			return nil
+		},
+		Name:  "dt(dev tool)",
+		Usage: "dev tool is a tool to simplify the daily use of commands for developers.",
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
